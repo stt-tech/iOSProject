267f81c0c9602858a2124b93d1d0396f158945ed
孙婷婷 <suntingting@etiaolong.com>
2017-06-06 18:34:08 +0800

    初始化项目

---
+++ iOSProject/iOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSProject
+//
+//  Created by Alpha on 2017/6/6.
+//  Copyright © 2017年 STT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSProject/iOSProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSProject
+//
+//  Created by Alpha on 2017/6/6.
+//  Copyright © 2017年 STT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
