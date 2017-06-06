e32af5d19b89ab4976b53d6b470c642a9a5279d7
孙婷婷 <suntingting@etiaolong.com>
2017-06-06 19:08:39 +0800

    修改呢项目目录

---
+++ iOSProject/iOSProject/iOSProject/Others/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  iOSProject
+//
+//  Created by Alpha on 2017/6/6.
+//  Copyright © 2017年 STT. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+
+
+@end
